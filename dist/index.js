56810b879be9ff4bc8a480729918f458c1cbddc7
YaroslavW <doctor355@yahoo.com>
2018-12-15 10:16:48 +0300

    Connection with BD

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,22 @@
-"use strict";
+'use strict';
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test1');
+var _mongoose = require('mongoose');
 
-// const Cat = mongoose.model('Cat', { name: String });
+var _mongoose2 = _interopRequireDefault(_mongoose);
 
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
+var _Post = require('./models/Post');
 
-var arr = [1, 2, 3, 4, 5, 6];
-var result = arr.map(function (value) {
-  return value * 2;
+var _Post2 = _interopRequireDefault(_Post);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_mongoose2.default.connect('mongodb://localhost/blog');
+
+var post = new _Post2.default({
+    title: "Первая запись",
+    text: "Привет Мир"
 });
-console.log(result);
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: true}));
-// app.use(bodyParser.json());
-
-// const posts = [
-//     {
-//         title:  "lorem ipsum1",
-//         text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum2",
-//         text: "Lorem ipsum dolor sit amet  Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum3",
-//         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum4",
-//         text: "Lorem ipsum dolor sitLorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     }    
-// ];
-
-// app.get('/posts', function(req, res) {
-//     return res.send(posts);
-// });
-
-// app.get('/posts/:id', function(req, res) {
-//     const id = req.params.id;
-//     res.send(posts[id]);
-// })
-// app.post('/posts', function(req, res) {
-//     const data = req.body;
-//     console.log(data);
-//     posts.push(data);
-//     return res.send(posts);
-// })
-// app.listen(3000, ()=> console.log('Server running on 3000 port'));
+post.save().then(function () {
+    return console.log('OK');
+});
--- src/index.js
+++ src/index.js
@@ -1,52 +1,12 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test1');
+import mongoose from 'mongoose';
 
-// const Cat = mongoose.model('Cat', { name: String });
+import PostModel from './models/Post';
 
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
+mongoose.connect('mongodb://localhost/blog');
 
-const arr = [1, 2, 3, 4, 5, 6];
-const result = arr.map(value => value * 2);
-console.log(result);
+const post = new PostModel({
+    title: "Первая запись",
+    text: "Привет Мир"
+});
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: true}));
-// app.use(bodyParser.json());
-
-// const posts = [
-//     {
-//         title:  "lorem ipsum1",
-//         text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum2",
-//         text: "Lorem ipsum dolor sit amet  Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum3",
-//         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     },
-//     {
-//         title:  "lorem ipsum4",
-//         text: "Lorem ipsum dolor sitLorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-//     }    
-// ];
-
-// app.get('/posts', function(req, res) {
-//     return res.send(posts);
-// });
-
-// app.get('/posts/:id', function(req, res) {
-//     const id = req.params.id;
-//     res.send(posts[id]);
-// })
-// app.post('/posts', function(req, res) {
-//     const data = req.body;
-//     console.log(data);
-//     posts.push(data);
-//     return res.send(posts);
-// })
-// app.listen(3000, ()=> console.log('Server running on 3000 port'));
+post.save().then(() => console.log('OK'));
